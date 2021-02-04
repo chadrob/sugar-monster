a7f99c82266a920f4bdcfd267d95d6b212d6da96
Chad James <robertsonchad1@gmail.com>
2021-02-03 20:07:34 -0600

    added sentry.io monitoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+Sentry.init({
+	dsn:
+		'https://0aeb358fd9e442e78e0de3d43e24f4ec@o505240.ingest.sentry.io/5622235',
+	integrations: [new Integrations.BrowserTracing()],
+
+	// We recommend adjusting this value in production, or using tracesSampler
+	// for finer control
+	tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
 	<BrowserRouter>
 		<App />
+		<button onClick={methodDoesNotExist}>Break the world</button>;
 	</BrowserRouter>,
 	document.getElementById('root')
 );
